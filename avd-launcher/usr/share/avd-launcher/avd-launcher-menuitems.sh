f37f313611dbbb4bfed89e5742417fa03e0fb1a4
Gustavo Moraes <gustavosotnas1@gmail.com>
2015-10-23 21:56:12 -0200

    Adicionado código do menu de opções do AVD Launcher, de acordo com o Gist: https://gist.github.com/gustavosotnas/0d30c1283d24b719d0cb

--- avd-launcher/usr/share/avd-launcher/avd-launcher-menuitems.sh
+++ avd-launcher/usr/share/avd-launcher/avd-launcher-menuitems.sh
@@ -5,3 +5,11 @@
 # This file is subject to the terms and conditions of the GNU General Public
 # License. See the file COPYING in the main directory of this archive
 # for more details.
+
+yad --center --icons --window-icon="android" \
+--item-width="150" --single-click --read-dir="./desktopfiles/" \
+--width="340" --height="430" \
+--title="Android Virtual Device Manager" \
+--name="Android Virtual Device Manager" \
+--button=About:"./avd-launcher-helper.sh about" \
+--button="Close:1"; # Deve ser executado na pasta (...)'/usr/share/avd-launcher/'

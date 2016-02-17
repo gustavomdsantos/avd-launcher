e110870e013cc078c33ac0e9bcf94acf8842244b
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-02-17 13:14:06 -0200

    Criadas referências ("import") para model/AppInfo.sh

--- avd-launcher/usr/share/avd-launcher/view/CLI.sh
+++ avd-launcher/usr/share/avd-launcher/view/CLI.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+shopt -s expand_aliases && alias import='alias'; # Permite usar "import" em .sh
+
+import model.AppInfo='source ../model/AppInfo.sh';
 
 # Author: Gustavo Moraes <gustavosotnas1@gmail.com>
 #
@@ -9,38 +12,29 @@
 # "Classe" que contêm todas as mensagens específicas para linha de comando (CLI)
 
 # Função que exibe um texto de ajuda sobre o aplicativo no Terminal (CLI):
-# Uma pequena descrição do funcionamento do aplicativo, bug report, a licença do aplicativo e seu autor.
-# Parâmetros:
-# 	$CMD_NAME (via 'get_message') - o nome comando para executar o aplicativo
-# 	$VERSION (via 'get_message')  a versão do aplicativo
-# 	$APP_AUTHOR (via 'get_message')  o texto de direitos de autor
-# 	$CONTACT_AUTHOR (via 'get_message')  URL de contato do autor
-# 	$APP_HOMEPAGE (via 'get_message')  URL para o código fonte do aplicativo na Internet
-# 	$APP_ABOUT_FMT (via 'get_message')  texto de descrição do aplicativo (formatado para CLI)
-#	$ADVICE_DESCRIPTION_TEXT (via 'get_message')  texto de aviso a respeito do AVD Manager, ferramenta que faz parte do Android SDK
+# Uma pequena descrição do funcionamento do aplicativo, bug report, 
+# a licença do aplicativo e seu autor.
 displayHelp()
 {
-	echo -e "\nUsage: `get_message CMD_NAME`";
-	echo -e "   or: `get_message CMD_NAME` [OPTION]\n";
+	echo -e "\nUsage: `model.AppInfo getCmdName`";
+	echo -e "   or: `model.AppInfo getCmdName` [OPTION]\n";
 
-	get_message APP_ABOUT_FMT;
-	get_message ADVICE_DESCRIPTION_TEXT;
+	echo -e "`model.AppInfo getAppAbout`" | fmt -t;
+	echo -e "`model.AppInfo getAppAdvice`";
 
 	echo -e "\nOptions:";
 	echo -e "  -h, --help			Display this help and exit";
 	echo -e "      --version			Shows version information and exit\n";
 
-	echo "Report `get_message CMD_NAME` bugs to <`get_message APP_HOMEPAGE`>";
+	echo "Report `model.AppInfo getCmdName` bugs to <`model.AppInfo getAppHomepage`>";
 	echo "Released under the GNU General Public License."
-	echo "`get_message APP_AUTHOR` <`get_message CONTACT_AUTHOR`>";
+	echo "`model.AppInfo getAppAuthor` <`model.AppInfo getAuthorContact`>";
 }
 
-# Função que exibe a versão do aplicativo na linha de comando - Terminal
-# Parâmetros:
-# 	$VERSION (variável GLOBAL) - a versão do aplicativo
+# Função que exibe a versão do aplicativo no Terminal (CLI).
 displayVersion()
 {
-	echo "$(get_message VERSION)";
+	echo "$(model.AppInfo getVersion)";
 }
 
 #### MAIN ####

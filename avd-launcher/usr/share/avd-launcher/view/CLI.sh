2a95dca5f98332217a5a15791698828a9f01eb95
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-02-17 12:29:50 -0200

    Criada view CLI, que contêm todas as mensagens específicas para linha de comando

---
+++ avd-launcher/usr/share/avd-launcher/view/CLI.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Author: Gustavo Moraes <gustavosotnas1@gmail.com>
+#
+# This file is subject to the terms and conditions of the GNU General Public
+# License. See the file COPYING in the main directory of this archive
+# for more details.
+
+# "Classe" que contêm todas as mensagens específicas para linha de comando (CLI)
+

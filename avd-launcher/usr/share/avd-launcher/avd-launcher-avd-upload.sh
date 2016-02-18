4d7dfaed82722c8693883e14b8aa821eb48ad07e
pauloXtr3m <paulo-gyn@outlook.com>
2016-02-18 12:10:06 -0200

    Alterações no avd-launcher-avd-upload.sh: - melhoria no layout - adicionada função que permite o usuário escolher a pasta do emulador que deseja enviar o arquivo

--- avd-launcher/usr/share/avd-launcher/avd-launcher-avd-upload.sh
+++ avd-launcher/usr/share/avd-launcher/avd-launcher-avd-upload.sh
@@ -1,38 +1,59 @@
 #! /bin/bash
-adb_device_name=`~/Android/Sdk/platform-tools/adb devices | grep emulator | xargs echo | cut -d' ' -f1`;
-send_file()
+adbDeviceName=`~/Android/Sdk/platform-tools/adb devices | grep emulator | xargs echo | cut -d' ' -f1`;
+sendFile()
 {
 
 	false; # Para entrar no while
 	while [ $? -ne 0 ] # Enquanto a saída do último comando não for igual a ZERO (return =! 0)
 	do
-		file_path_tmp1=$(choose_local_file);
+		filePathTmp1=$(chooseLocalFile);
 			verifyReturnCode;
 		if [ "$?" != "1" ] # Se o usuário não quer sair do programa
 		then
-			adb -s "$adb_device_name" push "$file_path_tmp1" /sdcard/
-			local returnCode=$?;
-			file_path_tmp1=""; #desaloca variável bash
-			generateReturnCode $returnCode; ### Aqui não pode ser usado o "return" diretamente porque iria finalizar o loop "while" (BASH bosta)
+		emulatorPathTmp1=$(chooseEmulatorPath);
+			verifyReturnCode;
+
+			if [ "$?" != "1" ] # Se o usuário não quer sair do programa
+			then
+				adbReturn=$(adb -s "$adbDeviceName" push "$filePathTmp1" "$emulatorPathTmp1");
+				local returnCode=$?;
+				filePathTmp1=""; #desaloca variável bash
+				emulatorPathTmp1=""; #desaloca variável bash
+				generateReturnCode $returnCode; ### Aqui não pode ser usado o "return" diretamente porque iria finalizar o loop "while" (BASH bosta)
+			else # $? == 1
+				false;
+			fi
 		else # $? == 1
 			false;
 		fi
 	done
 }
 
-choose_local_file(){
+chooseLocalFile(){
 
-	file_path_tmp2=$(yad --title "$APP_NAME" --form \
-	--center --width="800" --height="500" --image="android" --window-icon="android" \
+	filePathTmp2=$(yad --title "$APP_NAME" \
+	--center --width="800" --height="500" --window-icon="android" \
 		--image="/usr/share/pixmaps/avd-launcher/avd-upload.png" \
-		--file --multiple --file-filter="Todos os arquivos|*.*" \
-		--text "$HELP_DESCRIPTION_TEXT\n\n" \
+		--file --multiple --file-filter="Todos os arquivos|*.*" DIR $HOME --separator=""\
+		--text "\n\nChoose files to send\n\n" \
 		--button=About:"./avd-launcher-helper.sh about" --button=Cancel:"./avd-launcher-helper.sh cancel" --button=OK:0);
-		#process_return_cancel_button;
+		# process_return_cancel_button;
 	local returnCode=$?; # Armazena o return (variável "?") para retornar depois (variável local)
-	echo "$file_path_tmp2"; # "return"
-	file_path_tmp2=""; # desaloca variável bash
+	echo "$filePathTmp2"; # "return"
+	filePathTmp2=""; # desaloca variável bash
 	return $returnCode;
 }
+chooseEmulatorPath(){
+	emulatorPathTmp2=$(yad --title "$APP_NAME" --entry\
+		--center --width=500 --window-icon="android" \
+		--image="/usr/share/pixmaps/avd-launcher/avd-upload.png" \
+		--text "\n\nWrite the emulator path you want to send the file(Example: '/sdcard/'):\n" \
+		--entry \
+		--button=About:"./avd-launcher-helper.sh about" --button=Cancel:"./avd-launcher-helper.sh cancel" --button=OK:0);
+		# process_return_cancel_button;
+	local returnCode=$?; # Armazena o return (variável "?") para retornar depois (variável local)
+	echo "$emulatorPathTmp2"; # "return"
+	emulatorPathTmp2=""; # desaloca variável bash
+	return $returnCode;
 
-choose_local_file;
+}

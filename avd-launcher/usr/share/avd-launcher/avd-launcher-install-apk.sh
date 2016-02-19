473e6548cf67586dba290dd205ba05dd381e3fd9
RenanOfugi <renangyn2010@hotmail.com>
2016-02-19 01:24:50 -0200

    - Modificada ordem dos métodos - por algum motivo as variaveis ficavam de outra cor.

--- avd-launcher/usr/share/avd-launcher/avd-launcher-install-apk.sh
+++ avd-launcher/usr/share/avd-launcher/avd-launcher-install-apk.sh
@@ -1,13 +1,10 @@
 # Arquivo responsável pela instalação de qualquer APK desejado pelo usuário, do PC para o emulador.
-selectDirectory(){
+installAPK(){
 
-  local Origem=$(yad --file \
-  --directory \
-  --title="Selecione o diretório do ADB" \
-  --width="500" \
-  --height="600");
+  pathAPK = $(selectAPK);
+  selectDirectory;
 
-  adbDeviceName = $("$Origem" | grep emulator | xargs echo | cut -d' ' -f1`);
+  adb -s "$adbDeviceName" install "$pathAPK";
 }
 
 selectAPK(){
@@ -22,10 +19,13 @@ selectAPK(){
   return APK;
 }
 
-installAPK(){
+selectDirectory(){
 
-  pathAPK = $(selectAPK);
-  selectDirectory;
+  local Origem=$(yad --file \
+  --directory \
+  --title="Selecione o diretório do ADB" \
+  --width="500" \
+  --height="600");
 
-  adb -s "$adbDeviceName" install "$pathAPK";
+  adbDeviceName = $("$Origem" | grep emulator | xargs echo | cut -d' ' -f1`);
 }
